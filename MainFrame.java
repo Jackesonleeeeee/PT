f27f2975386dc0cc6a64aacc7efece6008c05b16
Jackesonleeeeee <43921884+Jackesonleeeeee@users.noreply.github.com>
2020-02-18 10:13:37 -0800

    update

--- MainFrame.java
+++ MainFrame.java
@@ -1,5 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
 class MainFrame{
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Player a=new Player();
+        JFrame begin=new JFrame("Begin game");
+        begin.setSize(900,900);
+        JButton play=new JButton("Play");
+        JPanel playp=new JPanel();
+        playp.add(play);
+        begin.add(playp,"South");
+        begin.setVisible(true);
+        play.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent evt){ 
+        startGame(a);
+    }
+    });
+    }
+    public static void startGame(Player a){
+        a.Player(getInitialFund());
+        System.out.println("You have $"+a.getMoney()+" to start with");
+        //homeScreen(a);
+    }
+    public static int getInitialFund(){
+        int fund=1000*((int)(10*Math.random())+1);
+        return fund;
     }
 }
